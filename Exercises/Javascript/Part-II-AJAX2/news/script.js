51e870c3126235025dbece8121eac24060413e63
Paulo Ribeiro <pjsalgadomribeiro@gmail.com>
2020-12-02 13:45:45 +0000

    Collect Data
    Javascript - Part II (AJAX2) - Exercise 5 (ii)

--- Exercises/Javascript/Part-II-AJAX2/news/script.js
+++ Exercises/Javascript/Part-II-AJAX2/news/script.js
@@ -3,5 +3,20 @@ commentForm.addEventListener('submit', submitComment)
 
 function submitComment(event) {
     event.preventDefault()
-    console.log("Function submitComment called!")
+
+    let id = commentForm.querySelector("input[name=id]").value
+    let comment_id;
+    try {
+        comment_id = document.querySelector("#comments article:last-of-type .id").textContent
+    }
+    catch {
+        comment_id = 0
+    }
+    let username = commentForm.querySelector("input[name=username]").value
+    let text = commentForm.querySelector("textarea[name=text]").value
+
+    console.log("ID: ", id)
+    console.log("Comment ID: ", comment_id)
+    console.log("Username: ", username)
+    console.log("Text: ", text)
 }
